b334cc2abafbb0e47dce98507356609ba70829a1
whbbit <luckyboy_dong@sina.com>
2023-01-09 01:41:10 +0800

    feat: 更新依赖项
    1. 项目依赖更新至最新版本
    2. dumi配置文件更新
    3. GitHub action 配置文件更新，测试自动部署后是否可以进行页面访问

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'dumi';
+// import { defineConfig } from 'dumi';
+// export default defineConfig({
+//   themeConfig: {
+//     name: 'SableChat',
+//   },
+// });
 
-export default defineConfig({
-  outputPath: 'docs-dist',
+export default {
   themeConfig: {
     name: 'SableChat',
   },
+  mfsu: true,
   base: '/sable-chat-ui',
   publicPath: '/sable-chat-ui/',
-});
+};
